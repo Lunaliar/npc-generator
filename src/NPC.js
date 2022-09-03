26bcbb025e2f7ef7565a0e8303c8c330ab876741
Jake Tripp <shmogens@gmail.com>
2022-09-03 15:08:49 -0700

    remove crap

--- src/NPC.js
+++ src/NPC.js
@@ -1,16 +1,8 @@
 import React, { Component } from 'react'
 import * as traits from "./traits"
 
-/**
- * Returns a random integer between min (inclusive) and max (inclusive).
- * The value is no lower than min (or the next integer greater than min
- * if min isn't an integer) and no greater than max (or the next integer
- * lower than max if max isn't an integer).
- * Using Math.round() will give you a non-uniform distribution!
- */
+/** Get random integer from inclusive range */
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
